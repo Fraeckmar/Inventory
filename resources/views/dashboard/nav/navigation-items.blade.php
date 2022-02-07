08cc7ff5bd9b829b54dca84a874eaef062bb8b33
Franz <franciscomaribao98@gmail.com>
2022-02-07 21:38:51 +0800

    enhance menu

--- resources/views/dashboard/nav/navigation-items.blade.php
+++ resources/views/dashboard/nav/navigation-items.blade.php
@@ -13,7 +13,8 @@
             </a>
             <ul class="sub-item">
                {{-- New Item --}}
-                <li class="mr-3 flex-1 p-2 sm:p-0">
+                <li class="mr-3 flex-1 p-2 relative sm:p-0">
+                    <span class="absolute hidden sm:block left-1.5 -top-2"><i class="fas fa-sort-up text-purple-400"></i></span>
                     <a href="{{ url('/items/create') }}" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-purple-400 @if(request()->is('items/create')) text-purple-400 @endif">
                         <i class="fas fa-plus pr-0 md:pr-3 fa-2x h-5"></i><span class="inline pb-1 md:pb-0 text-sm md:text-base">{{ __('New Item') }}</span>
                     </a>
@@ -45,7 +46,8 @@
             </a>
             <ul class="sub-item">
                 {{-- New User --}}
-                 <li class="mr-3 flex-1 p-2 sm:p-0">
+                 <li class="mr-3 flex-1 p-2 relative sm:p-0">
+                    <span class="absolute hidden sm:block left-1.5 -top-2"><i class="fas fa-sort-up text-purple-400"></i></span>
                      <a href="{{ url('/register') }}" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-purple-400 @if(request()->is('register')) text-purple-400 @endif">
                          <i class="fas fa-user-plus pr-0 md:pr-3 fa-2x h-5"></i><span class="inline pb-1 md:pb-0 text-sm md:text-base">{{ __('New Customer') }}</span>
                      </a>
