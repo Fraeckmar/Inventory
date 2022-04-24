0aa3d4294f65cc624ff385ede8a4adee62aeff3f
Franz <franciscomaribao98@gmail.com>
2022-04-24 18:09:38 +0800

    fix items table actions responsice

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,7 +1,7 @@
 @extends('dashboard.index')
 
 @section('content')
-<section class="container mx-auto sm:p-6 font-mono">
+<section class="container mx-auto p-0 sm:p-0 font-mono">
 	<div class="row">
 		<div class="w-full mb-8 overflow-auto md:overflow-hidden rounded-lg shadow-lg">
 			{!! $dataTable->draw() !!}
