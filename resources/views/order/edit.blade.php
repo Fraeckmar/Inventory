a288bdc03691fbcd4871c10c7cb6202e72565cb0
Franz <franciscomaribao98@gmail.com>
2022-04-26 02:02:56 +0800

    fix edit order hading

--- resources/views/order/edit.blade.php
+++ resources/views/order/edit.blade.php
@@ -5,7 +5,7 @@
 	{{ GenField::notification(session('success'), 'success') }}
 @endif
 <div class="bg-white shadow-md border border-gray-200 rounded-sm mx-auto mt-4 w-full sm:w-full md:w-2/3">
-	<h1 class="bg-blue-100 block w-full text-center text-xl font-medium uppercase p-4">{{ __('Edit Item') }}</h1>
+	<h1 class="bg-blue-100 block w-full text-center text-xl font-medium uppercase p-4">{{ __('Edit Order') }}</h1>
 	<div class="p-4">
 		{!! Form::open(['action'=>['App\Http\Controllers\ItemBoundController@update',$itemBound->id], 'method'=>'put']) !!}
 			@foreach ( Field::boundFields('outbound') as $field )
