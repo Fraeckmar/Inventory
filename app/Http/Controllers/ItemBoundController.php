98351ffb2469242062478f2a44289560e7d7fcca
Franz <franciscomaribao98@gmail.com>
2022-05-03 23:47:59 +0800

    Enhancement

--- app/Http/Controllers/ItemBoundController.php
+++ app/Http/Controllers/ItemBoundController.php
@@ -516,7 +516,7 @@ class ItemBoundController extends Controller
             $date_from = date('Y-m-d', strtotime($request->date_from));
             $date_to = date('Y-m-d', strtotime('+1 day', strtotime($request->date_to)));
             $where_clase .= !empty($where_clase) ? " AND" : "";
-            $where_clase .= " item_bounds.created_at BETWEEN CAST('{$date_from}' AS DATE) AND  CAST('{$date_to}' AS DATE)";
+            $where_clase .= " item_bounds.created_at BETWEEN TIMESTAMP('{$date_from}') AND  TIMESTAMP('{$date_to}')";
         }
         if ($request->has('customer') && !empty($request->customer)) {
             $where_clase .= !empty($where_clase) ? " AND" : "";
